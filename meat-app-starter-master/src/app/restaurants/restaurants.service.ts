63a342fc636f39ae893add8a09c60b12fb3e90ee
Paulo Henrique Correa dos Santos <phcorreacontato@gmail.com>
2019-08-09 11:12:18 -0300

    Implementação de ErrorHandler para o RestaurantsService

--- meat-app-starter-master/src/app/restaurants/restaurants.service.ts
+++ meat-app-starter-master/src/app/restaurants/restaurants.service.ts
@@ -3,8 +3,10 @@ import { Http } from '@angular/http';
 import {Restaurant} from './restaurant/restaurant.model'
 
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
 import { MEAT_API } from '../app.api'
 import { Observable } from 'rxjs/Observable';
+import { ErrorHandler } from '../app.error-handler';
 
 
 @Injectable()
@@ -13,6 +15,8 @@ export class RestaurantsService {
     constructor(private http: Http) {}
 
     restaurants(): Observable<Restaurant[]> {
-        return this.http.get(`${MEAT_API}/restaurants`).map(response => response.json())
+        return this.http.get(`${MEAT_API}/restaurants`)
+          .map(response => response.json())
+          .catch(ErrorHandler.handlererror)
     }
 }
